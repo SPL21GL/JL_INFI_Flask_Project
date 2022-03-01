e07c0c699729bef9ec988f9bd9ede158271b86db
jakobSPL2021 <jakob.laurin@bulme.at>
2022-03-01 09:05:06 +0100

    Connection db flask

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-print("hello World")
-print("this is a test")
+from flask import Flask, redirect, request, flash, session
+from flask.templating import render_template
+from sqlalchemy import update
+from models import db
+
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/businessapp"
+print("test")
+
+db.init_app(app)
---
+++ models.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+
+class Abteilung(db.Model):
+    __tablename__ = 'abteilung'
+
+    AbteilungsID = db.Column(db.Integer, primary_key=True, unique=True)
+    Name = db.Column(db.String(64), nullable=False)
+    Gebäude = db.Column(db.Integer, nullable=False)
+
+
+
+class Arbeitsgruppe(db.Model):
+    __tablename__ = 'arbeitsgruppe'
+
+    ArbeitsgruppenID = db.Column(db.Integer, primary_key=True, unique=True)
+    Name = db.Column(db.String(64), nullable=False)
+    Raum = db.Column(db.Integer, nullable=False)
+
+
+
+class ArbeitsgruppeMitarbeiter(db.Model):
+    __tablename__ = 'arbeitsgruppe_mitarbeiter'
+
+    Arbeitsgruppe_MitarbeiterID = db.Column(db.Integer, primary_key=True, unique=True)
+    ArbeitsgruppenID = db.Column(db.ForeignKey('arbeitsgruppe.ArbeitsgruppenID'), index=True)
+    MitarbeiterID = db.Column(db.ForeignKey('mitarbeiter.MitarbeiterID'), index=True)
+
+    arbeitsgruppe = db.relationship('Arbeitsgruppe', primaryjoin='ArbeitsgruppeMitarbeiter.ArbeitsgruppenID == Arbeitsgruppe.ArbeitsgruppenID', backref='arbeitsgruppe_mitarbeiters')
+    mitarbeiter = db.relationship('Mitarbeiter', primaryjoin='ArbeitsgruppeMitarbeiter.MitarbeiterID == Mitarbeiter.MitarbeiterID', backref='arbeitsgruppe_mitarbeiters')
+
+
+
+class Mitarbeiter(db.Model):
+    __tablename__ = 'mitarbeiter'
+
+    MitarbeiterID = db.Column(db.Integer, primary_key=True, unique=True)
+    Voname = db.Column(db.String(32), nullable=False)
+    Nachname = db.Column(db.String(32))
+    Lohn = db.Column(db.Integer, nullable=False)
+    Adresse = db.Column(db.String(64))
+    Beschäftigung = db.Column(db.String(64))
+    Geburtsdatum = db.Column(db.Date)
