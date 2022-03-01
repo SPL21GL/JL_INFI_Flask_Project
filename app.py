3e90283c7df9d6f994c78468b104002766ac5539
jakobSPL2021 <jakob.laurin@bulme.at>
2022-03-01 10:30:36 +0100

    Final commit today

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, redirect, request, flash, session
-from flask.templating import render_template
+from flask import Flask, redirect, request, flash, session, render_template
 from sqlalchemy import update
 from models import db
 
@@ -8,6 +7,13 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/businessapp"
-print("test")
 
 db.init_app(app)
+app.secret_key = "VerSecretSecretKey"
+
+@app.route("/", methods=["get","post"])
+
+#Show Hello World on index page
+def index():
+    return render_template("index.html")
+app.run(debug=True)
