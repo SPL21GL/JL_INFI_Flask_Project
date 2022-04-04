b1a585f19decaf365eb50ae0b2c5714168f3c7aa
jakobSPL2021 <jakob.laurin@bulme.at>
2022-04-04 16:18:13 +0200

    addWorkgroup funktioniert

---
+++ forms/AddForms/addWorkgroupForm.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms.fields.simple import StringField
+from wtforms.fields import DecimalField
+from wtforms import validators
+
+class AddWorkgroupsForm(FlaskForm):
+    Name = StringField("Name",validators = [validators.Length(3,100),validators.input_required()])
+    Raum = DecimalField("Room",validators = [validators.input_required()])
